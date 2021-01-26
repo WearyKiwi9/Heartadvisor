bbb51bedbdbf43e45478a64acf1447f43fb4b02c
Tejal Patel <43835967+WearyKiwi9@users.noreply.github.com>
2021-01-26 11:32:57 -0800

    Initial Setups

---
+++ Heartadvisor/Constants.swift
@@ -0,0 +1,30 @@
+//
+//  Constants.swift
+//  Heartadvisor
+//
+//  Created by Tejal Patel on 1/26/21.
+//
+
+import Foundation
+
+struct Constants {
+    //Color variables
+    static let syncMainColor = "#264b77";
+    static let syncYellowColor = "#b67929";
+    static let syncGrayColor = "#a3acb1";
+    static let syncDarkBlueColor = "#00243f";
+
+    // Font variables
+    // Typically for headers
+    static let xlargeFont = "SFProDisplay-Bold"
+    static let xlargeFontSize = 34
+    // Typically for Buttons/Sub-headers
+    static let largeFont = "SFProDisplay-Medium"
+    static let largeFontSize = 20
+    // Typically for paragraphs/regular text
+    static let smallFont = "SFProDisplay-Regular"
+    static let smallFontSize = 16
+    // Typically for captions/footnote
+    static let xsmallFont = "SFProDisplay-Thin"
+    static let xsmallFontSize = 12
+}
