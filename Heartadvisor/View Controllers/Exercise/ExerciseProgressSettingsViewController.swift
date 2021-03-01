8d55f5156a6a59e19458691187310f8c5cd213ea
Andrew Tsui <andrew3492@gmail.com>
2021-02-28 19:29:24 -0800

    Merged Exercise

---
+++ Heartadvisor/View Controllers/Exercise/ExerciseProgressSettingsViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ExerciseProgressSettingsViewController.swift
+//  Heartadvisor
+//
+//  Created by Andrew Tsui on 2/28/21.
+//
+
+import Foundation
---
+++ Heartadvisor/View Controllers/Exercise/ExerciseViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ExerciseViewController.swift
+//  Heartadvisor
+//
+//  Created by Andrew Tsui on 2/28/21.
+//
+
+import Foundation
